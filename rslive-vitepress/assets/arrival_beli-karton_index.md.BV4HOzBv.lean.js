2d2798b469a2abfaa7a66e74ba7c98994c59f871
Developer <dev@example.com>
2026-04-28 00:31:25 +0200

    Add uplatnica prototypes for Astro and VitePress

---
+++ rslive-vitepress/assets/arrival_beli-karton_index.md.BV4HOzBv.lean.js
@@ -0,0 +1,13 @@
+import { _ as _export_sfc, o as openBlock, c as createElementBlock, aj as createStaticVNode } from "./chunks/framework.CTBQRFhE.js";
+const __pageData = JSON.parse('{"title":"Белый картон: регистрация иностранца в Сербии","description":"Белый картон** — это документ, который подтверждает место пребывания иностранца в Сербии. Официально он называется prijava borovka stranca. Он необходим для законного нахождения в ","frontmatter":{"title":"Белый картон: регистрация иностранца в Сербии","description":"Белый картон** — это документ, который подтверждает место пребывания иностранца в Сербии. Официально он называется prijava borovka stranca. Он необходим для законного нахождения в ","tags":["белый картон","регистрация иностранца","Сербия","prijava boravka stranca","легальность","документы","еУправа","аренда","ВНЖ"]},"headers":[],"relativePath":"arrival/beli-karton/index.md","filePath":"arrival/beli-karton/index.md","lastUpdated":null}');
+const _sfc_main = { name: "arrival/beli-karton/index.md" };
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  return openBlock(), createElementBlock("div", null, [..._cache[0] || (_cache[0] = [
+    createStaticVNode("", 18)
+  ])]);
+}
+const index = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render]]);
+export {
+  __pageData,
+  index as default
+};
---
+++ rslive-vitepress/assets/integration_stalni_index.md.C4uxuUsN.lean.js
@@ -0,0 +1,13 @@
+import { _ as _export_sfc, o as openBlock, c as createElementBlock, aj as createStaticVNode } from "./chunks/framework.CTBQRFhE.js";
+const __pageData = JSON.parse('{"title":"Получениe ПМЖ","description":"Уточнить допустимый срок отсутствия Указать про необходимость регистрации при путешествиях по Сербии с остановкой более 2 недель Ссылки на оригинальные законы и правильники Квитанц","frontmatter":{"title":"Получениe ПМЖ","description":"Уточнить допустимый срок отсутствия Указать про необходимость регистрации при путешествиях по Сербии с остановкой более 2 недель Ссылки на оригинальные законы и правильники Квитанц"},"headers":[],"relativePath":"integration/stalni/index.md","filePath":"integration/stalni/index.md","lastUpdated":null}');
+const _sfc_main = { name: "integration/stalni/index.md" };
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  return openBlock(), createElementBlock("div", null, [..._cache[0] || (_cache[0] = [
+    createStaticVNode("", 21)
+  ])]);
+}
+const index = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render]]);
+export {
+  __pageData,
+  index as default
+};
