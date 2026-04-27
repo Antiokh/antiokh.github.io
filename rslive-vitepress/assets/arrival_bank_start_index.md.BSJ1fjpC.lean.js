f2bfec76163ce658577b4b47774d5c7eb14bc19f
Developer <dev@example.com>
2026-04-28 00:37:39 +0200

    Match uplatnica prototypes to DokuWiki plugin

---
+++ rslive-vitepress/assets/arrival_bank_start_index.md.BSJ1fjpC.lean.js
@@ -0,0 +1,13 @@
+import { _ as _export_sfc, o as openBlock, c as createElementBlock, ag as createStaticVNode } from "./chunks/framework.Cew7L-Hp.js";
+const __pageData = JSON.parse('{"title":"Банки и финансы","description":"Собрать информацию про API Banka (открытие, комиссии, порядок) → API Banka Собрать информацию про Yettel Bank (открытие, комиссии, порядок) → Yettel Bank После переезда в Сербию од","frontmatter":{"title":"Банки и финансы","description":"Собрать информацию про API Banka (открытие, комиссии, порядок) → API Banka Собрать информацию про Yettel Bank (открытие, комиссии, порядок) → Yettel Bank После переезда в Сербию од","tags":["банки в Сербии","открытие банковского счета","переводы денег из России","карты UnionPay","финансовые операции в Сербии","комиссии за обслуживание","налоги на банковские операции"]},"headers":[],"relativePath":"arrival/bank/start/index.md","filePath":"arrival/bank/start/index.md","lastUpdated":null}');
+const _sfc_main = { name: "arrival/bank/start/index.md" };
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  return openBlock(), createElementBlock("div", null, [..._cache[0] || (_cache[0] = [
+    createStaticVNode("", 12)
+  ])]);
+}
+const index = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render]]);
+export {
+  __pageData,
+  index as default
+};
