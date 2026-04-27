f94f0642fd70b89cd789fee6f92236d813306a49
Developer <dev@example.com>
2026-04-28 00:16:58 +0200

    Add rslive static site prototypes

---
+++ rslive-vitepress/assets/adaptation_cef_index.md.DIzQh_DP.lean.js
@@ -0,0 +1,13 @@
+import { _ as _export_sfc, o as openBlock, c as createElementBlock, ag as createStaticVNode } from "./chunks/framework.Cew7L-Hp.js";
+const __pageData = JSON.parse('{"title":"ЭЦП, картицы, читач, челик","description":"Водительские права (возачка дозвола), карточка ОМС (здравствена картица) и ID (лична картица) имеют чип, являются смарт-картами и могут быть прочитаны через специальное устройство.","frontmatter":{"title":"ЭЦП, картицы, читач, челик","description":"Водительские права (возачка дозвола), карточка ОМС (здравствена картица) и ID (лична картица) имеют чип, являются смарт-картами и могут быть прочитаны через специальное устройство."},"headers":[],"relativePath":"adaptation/cef/index.md","filePath":"adaptation/cef/index.md","lastUpdated":null}');
+const _sfc_main = { name: "adaptation/cef/index.md" };
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  return openBlock(), createElementBlock("div", null, [..._cache[0] || (_cache[0] = [
+    createStaticVNode("", 51)
+  ])]);
+}
+const index = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render]]);
+export {
+  __pageData,
+  index as default
+};
---
+++ rslive-vitepress/assets/adaptation_start_index.md.BRBDu-yF.lean.js
@@ -0,0 +1,13 @@
+import { _ as _export_sfc, o as openBlock, c as createElementBlock, ag as createStaticVNode } from "./chunks/framework.Cew7L-Hp.js";
+const __pageData = JSON.parse('{"title":"3. Адаптация","description":"Адаптация в новой стране** — это долгий и многослойный процесс, включающий как внешние изменения (культура, язык), так и внутренние, связанные с самоощущением и идентичностью. Пере","frontmatter":{"title":"3. Адаптация","description":"Адаптация в новой стране** — это долгий и многослойный процесс, включающий как внешние изменения (культура, язык), так и внутренние, связанные с самоощущением и идентичностью. Пере"},"headers":[],"relativePath":"adaptation/start/index.md","filePath":"adaptation/start/index.md","lastUpdated":null}');
+const _sfc_main = { name: "adaptation/start/index.md" };
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  return openBlock(), createElementBlock("div", null, [..._cache[0] || (_cache[0] = [
+    createStaticVNode("", 26)
+  ])]);
+}
+const index = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render]]);
+export {
+  __pageData,
+  index as default
+};
