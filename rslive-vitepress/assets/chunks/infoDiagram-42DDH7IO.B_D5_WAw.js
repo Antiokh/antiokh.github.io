136c4d57686e79bb0a9ceef08ad974eb50cd9126
Developer <dev@example.com>
2026-04-28 12:46:44 +0200

    Add OpenGraph metadata to VitePress prototype

---
+++ rslive-vitepress/assets/chunks/infoDiagram-42DDH7IO.B_D5_WAw.js
@@ -0,0 +1,34 @@
+import { _ as __name, l as log, L as selectSvgElement, e as configureSvgSize } from "./theme.Bqji57aX.js";
+import { p as parse } from "./wardley-RL74JXVD.Bfk0J9H8.js";
+import "./framework.68bDsMbm.js";
+import "./min.Dwr35PN-.js";
+import "./baseUniq.CUhvsmx5.js";
+var parser = {
+  parse: /* @__PURE__ */ __name(async (input) => {
+    const ast = await parse("info", input);
+    log.debug(ast);
+  }, "parse")
+};
+var DEFAULT_INFO_DB = {
+  version: "11.14.0"
+};
+var getVersion = /* @__PURE__ */ __name(() => DEFAULT_INFO_DB.version, "getVersion");
+var db = {
+  getVersion
+};
+var draw = /* @__PURE__ */ __name((text, id, version) => {
+  log.debug("rendering info diagram\n" + text);
+  const svg = selectSvgElement(id);
+  configureSvgSize(svg, 100, 400, true);
+  const group = svg.append("g");
+  group.append("text").attr("x", 100).attr("y", 40).attr("class", "version").attr("font-size", 32).style("text-anchor", "middle").text(`v${version}`);
+}, "draw");
+var renderer = { draw };
+var diagram = {
+  parser,
+  db,
+  renderer
+};
+export {
+  diagram
+};
---
+++ rslive-vitepress/assets/chunks/stateDiagram-v2-QKLJ7IA2.CIwQecqj.js
@@ -0,0 +1,22 @@
+import { s as styles_default, b as stateRenderer_v3_unified_default, a as stateDiagram_default, S as StateDB } from "./chunk-OYMX7WX6.DLLTdWdo.js";
+import { _ as __name } from "./theme.Bqji57aX.js";
+import "./chunk-55IACEB6.CTwRL3Vy.js";
+import "./chunk-EDXVE4YY.DVBgSear.js";
+import "./framework.68bDsMbm.js";
+var diagram = {
+  parser: stateDiagram_default,
+  get db() {
+    return new StateDB(2);
+  },
+  renderer: stateRenderer_v3_unified_default,
+  styles: styles_default,
+  init: /* @__PURE__ */ __name((cnf) => {
+    if (!cnf.state) {
+      cnf.state = {};
+    }
+    cnf.state.arrowMarkerAbsolute = cnf.arrowMarkerAbsolute;
+  }, "init")
+};
+export {
+  diagram
+};
