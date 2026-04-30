a836cf65929bfb648d47924af6935444e85e13e3
Developer <dev@example.com>
2026-04-30 11:38:49 +0200

    Restyle panel toggles

---
+++ rslive-vitepress/assets/chunks/stateDiagram-v2-QKLJ7IA2.C61dA2CR.js
@@ -0,0 +1,22 @@
+import { s as styles_default, b as stateRenderer_v3_unified_default, a as stateDiagram_default, S as StateDB } from "./chunk-OYMX7WX6.C4scqK0J.js";
+import { _ as __name } from "./theme.CfIpBOkW.js";
+import "./chunk-55IACEB6.1i-tDzgn.js";
+import "./chunk-EDXVE4YY.BJUmzyad.js";
+import "./framework.CIANDS-T.js";
+var diagram = {
+  parser: stateDiagram_default,
+  get db() {
+    return new StateDB(2);
+  },
+  renderer: stateRenderer_v3_unified_default,
+  styles: styles_default,
+  init: /* @__PURE__ */ __name((cnf) => {
+    if (!cnf.state) {
+      cnf.state = {};
+    }
+    cnf.state.arrowMarkerAbsolute = cnf.arrowMarkerAbsolute;
+  }, "init")
+};
+export {
+  diagram
+};
